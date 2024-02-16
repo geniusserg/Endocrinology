95a014958082a5c1eda9dd7296df824bcaa44d7b
geniusserg <danilovsergeydmitrievich@gmail.com>
2024-02-17 00:21:29 +0300

    UI changes: result div, shap, params. Loaded new model

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 ## Application for obesity treatment 
 ## Sergey Danilov
-
-
-from ui.gui import View
 from ml.modelXGBoostClassifier import ModelXGBoostClassifier
 import json
+from flask import Flask, render_template, request, send_from_directory
+import os 
+import shap 
 
-config = json.load("config.json")
+app = Flask(__name__,  template_folder='templates')
 
-m = ModelXGBoostClassifier(model_path="model_snapshots/model.pkl")
-sample_data = {'OXC': 6.13, 'ДАД': 70.0, 'Возраст': 39.0, 'Лептин (нг/мл) 0 мес': 92.63}
-print(m.predict(data=sample_data))
 
+def run_model(input_data):
+    
+    return model.predict(data=input_data)
+
+@app.route('/', methods=['GET'])
+def index():
+    sample_data = config["sample_data"]
+    fields = [{"name": i, "placeholder": "Введите значение", "default": sample_data[i], "value": sample_data[i] } for i in sample_data] 
+    print(fields)
+    return render_template('index.html', fields = fields)
+
+@app.route('/', methods=['POST'])
+def submit():
+    data = request.form
+    # sample_data = config["sample_data"]
+    fields = [{"name": i, "placeholder": "Введите значение", "default": sample_data[i], "value": sample_data[i] } for i in sample_data] 
+    for field in fields:
+        field['value'] = data.get(field['name'], '')
+    input_data = {}
+    print("DATA: ", data)
+    for p in data:
+        print(p)
+        input_data[p] = float(data[p])
+    result, confidence = run_model(input_data)
+    return render_template('index.html', fields = fields, result = result, confidence = round(confidence*100, 2))
+
+
+if __name__ == '__main__':
+    # model test
+    config = json.load(open("config.json", "r", encoding="utf-8"))
+    model_path = os.path.join("model_snapshots", config["model_path"])
+    model = ModelXGBoostClassifier(model_path = model_path)
+
+    sample_data = config["sample_data"]
+    print(model.predict(sample_data))
+
+    #application
+    app.run(debug=True)
--- ui/gui.py
+++
@@ -1,9 +0,0 @@
-import shap
-
-# TODO: plan this class
-class View:
-    def __init__(self, state):
-        pass
-
-    def shap_summary_plot(self):
-        pass
