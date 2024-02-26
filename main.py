cd440de23c8dfe679d754096b673d87a58b7a8b4
geniusserg <danilovsergeydmitrievich@gmail.com>
2024-02-26 17:57:37 +0300

    Update model saving code. Add generation of partial dep. Model: 71 ROCAUC

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 ## Sergey Danilov
 from ml.model import Model
 import json
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, url_for
 import os 
 import shap 
+import pandas as pd
+import shutil
 
 app = Flask(__name__,  template_folder='templates')
 
@@ -15,38 +17,49 @@ model: Model = None
 def load_global_config(config_json_path = "config.json", model_snapshots_dir = "model_snapshots"):
     global config, model
     config = json.load(open(config_json_path, "r", encoding="utf-8"))
-    model_path = os.path.join(model_snapshots_dir, config["model_path"])
-    explainer_path = os.path.join(model_snapshots_dir, config["explainer_path"])
-    model = Model(model_path = model_path, explainer_path=explainer_path)
+    snapshot_name = config["model_path"]
+    classifier_path = os.path.join(model_snapshots_dir, snapshot_name, "model.pkl")
+    explainer_path = os.path.join(model_snapshots_dir, snapshot_name, "explainer.pkl")
+    data_path = os.path.join(model_snapshots_dir, snapshot_name, "data.csv")
+    shap_overall_plot = os.path.join(model_snapshots_dir, snapshot_name, "shap_overall.jpg")
+
+    model = Model(model_path = classifier_path, explainer_path = explainer_path)
+    if (os.path.exists(shap_overall_plot)):
+        shutil.copyfile(shap_overall_plot, os.path.join("static", "plot.jpg"))
 
     # small test on Model work
     sample_data = config["sample_data"]
     model.predict(sample_data)
 
+    config["last_data"] = None
+
 
 def run_model(input_data):
-    return model.predict(data=input_data)
-
+    return model.predict(input_data)
 
 def get_explanation(input_data):
     return model.explain(input_data)
 
+def get_partial(feature_a, feature_b=None):
+    return model.partial_explain(feature_a, feature_b)
 
 @app.route('/', methods=['GET'])
 def index():
-    sample_data = config["sample_data"]
-    fields = [{"name": i, "placeholder": "Введите значение", "default": sample_data[i], "value": sample_data[i] } for i in sample_data] 
-    print(fields)
-    return render_template('index.html', fields = fields)
+    if (config["last_data"]  is not None):
+        fields = config["last_data"] 
+    else:
+        sample_data = config["sample_data"]
+        fields = [{"name": i, "placeholder": "Введите значение", "default": sample_data[i], "value": sample_data[i] } for i in sample_data] 
+    config["last_data"] = fields
+    return render_template('index.html', fields = fields, features = config["features"])
 
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def submit():
     data = request.form
-    sample_data = config["sample_data"]
-    fields = [{"name": i, "placeholder": "Введите значение", "default": sample_data[i], "value": sample_data[i] } for i in sample_data] 
-    for field in fields:
-        field['value'] = data.get(field['name'], '')
+    fields = config["last_data"]
+    for idx, field in enumerate(config["features"]):
+        fields[idx]['value'] = data.get(field, '')
     input_data = {}
     for p in data:
         if data[p] != '':
@@ -60,7 +73,24 @@ def submit():
     result, confidence = run_model(input_data)
     shap_plot = get_explanation(input_data)
     shap_frame = f"<head>{shap.getjs()}</head><body>{shap_plot.html()}</body>"
-    return render_template('index.html', fields = fields, result = result, confidence = round(confidence*100, 2), shap_plot = shap_frame)
+    config["last_data"] = fields
+    config["last_shap_plot"] = shap_frame
+    config["last_result"] = result
+    config["last_confidence"] = confidence
+    return render_template('index.html', fields = fields, result = result, confidence = round(confidence*100, 2), shap_plot = shap_frame, features = config["features"])
+
+# @app.route('/explain', methods=['POST'])
+# def submit():
+#     data = request.form
+#     feature_a = get_partial(data.get("feature1", ""))
+#     feature_b = get_partial(data.get("feature2", None))
+#     get_partial(feature_a, feature_b)
+#     return render_template('index.html', fields = config["last_data"], 
+#                         result = config["last_result"],
+#                         confidence = round( config["last_confidence"]*100, 2),
+#                         shap_plot = config["last_shap_plot"],
+#                         features = config["features"],
+#                         )
 
 
 if __name__ == '__main__':
--- ml/model.py
+++ ml/model.py
@@ -1,24 +1,38 @@
 import pandas as pd
 import pickle
 import shap
+from matplotlib import pyplot as plt
 
 class Model():
     def __init__(self, model_path, explainer_path):
         with open(model_path, "rb") as f:
             self.model = pickle.load(f)
-
         with open(explainer_path, "rb") as f:
             self.explainer = pickle.load(f)
 
-    def predict(self, data):
+    def predict(self, data_input):
+        data = {k:data_input[k] for k in self.model.feature_names_in_}
         data = pd.DataFrame([data]).astype(float)
         y_pred = self.model.predict(data)[0]
         pred_prob = self.model.predict_proba(data)[0][y_pred]
+        print(y_pred, pred_prob)
         return y_pred, pred_prob
 
-    def explain(self, sample):
-        if (isinstance(sample, dict)):
-            sample = pd.DataFrame(sample, index=[0])
-        shap_values = self.explainer(sample)
+    def explain(self, data_input):
+        data = {k:data_input[k] for k in self.model.feature_names_in_}
+        data = pd.DataFrame(data, index=[0])
+        shap_values = self.explainer(data)
         shapplot = shap.plots.force(shap_values)
         return shapplot
+
+    def partial_explain(self, feature_a, feature_b=None):
+        shap_values = self.explainer(self.explainer.X)
+        shap.dependence_plot(
+            feature_a,
+            shap_values,
+            self.explainer.X,
+            show=False,
+            interaction_index=feature_b
+        )
+        plt.tight_layout()
+        plt.savefig("partial_shap_plot.jpg")
