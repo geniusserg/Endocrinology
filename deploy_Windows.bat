9911c311fd9e11571408f0299a059496f104bd3f
geniusserg <50704370+geniusserg@users.noreply.github.com>
2024-02-29 17:30:52 +0300

    Windows startup script

---
+++ deploy_Windows.bat
@@ -0,0 +1,9 @@
+if exist venv (
+    venv\Scripts\activate
+    python main.py
+) else (
+    python -m venv venv
+    venv\Scripts\activate
+    pip install -r requirements.txt
+    python main.py
+)
